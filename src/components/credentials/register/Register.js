fff4c9da0b43cf55ad15dfcbb3418d14aea167c0
Nirma Perera <nirmaperera23@gmail.com>
2019-10-18 14:33:41 -0400

    password and email verification

--- src/components/credentials/register/Register.js
+++ src/components/credentials/register/Register.js
@@ -2,6 +2,51 @@ import React, { Component } from 'react'
 import './register.css';
 
 class Register extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstName: "",
+            lastName: "",
+            userId: "",
+            ssn: "",
+            email: "",
+            verifyEmail: "",
+            password: "",
+            verifyPassword: "",
+            securityAnswer: "",
+            zipCode: "",
+            verify: "* The fields do not match",
+            visible: true,
+            showError: false
+        }
+    }
+
+    handleUserInput = (event) => {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("Signing Up")
+    }
+
+    /** Verification */
+    Verification = (evt) => {
+        evt.preventDefault()
+        if ((this.state.password !== this.state.verifyPassword) || (this.state.email !== this.state.verifyEmail)) {
+            this.setState({
+                visible: true,
+                showError: true
+            })
+        }
+        else {
+            this.setState({
+                visible: false,
+                showError: false
+            })
+
+        }
+    }
     render() {
         return (
             <form className="base-container" ref={this.props.containerRef}>
@@ -11,20 +56,20 @@ class Register extends Component {
                     <div className="form">
                         <div className="form-group-reg">
                             <div className="form-row-reg">
-                                <input type="text" name="FirstName" placeholder="First Name" required />
+                                <input type="text" name="firstName" placeholder="First Name" required onChange={this.handleUserInput} />
                             </div>
                             <div className="form-row-reg">
-                                <input type="text" name="lastName" placeholder="Last Name" required />
+                                <input type="text" name="lastName" placeholder="Last Name" required onChange={this.handleUserInput} />
                             </div>
 
                         </div>
                         <div className="form-group-reg">
 
                             <div className="form-row-reg">
-                                <input type="text" name="userId" placeholder="User ID" required />
+                                <input type="text" name="userId" placeholder="User ID" required onChange={this.handleUserInput} />
                             </div>
                             <div className="form-row-reg">
-                                <input type="number" name="ssn" placeholder="ssn (last 4 digits)" required />
+                                <input type="number" name="ssn" placeholder="ssn (last 4 digits)" required onChange={this.handleUserInput} />
                             </div>
                         </div>
 
@@ -32,20 +77,20 @@ class Register extends Component {
 
 
                             <div className="form-row-reg">
-                                <input type="password" name="password" placeholder="password" required />
+                                <input type="password" name="password" placeholder="password" required onChange={this.handleUserInput} />
                             </div>
                             <div className="form-row-reg">
-                                <input type="password" name="password2" placeholder="confirm password" required />
+                                <input type="password" name="verifyPassword" placeholder="confirm password" required onChange={this.handleUserInput} onKeyUp={this.Verification} />
                             </div>
                         </div>
 
 
                         <div className="form-group-reg">
                             <div className="form-row-reg">
-                                <input type="email" name="email" placeholder="email" required />
+                                <input type="email" name="email" placeholder="email" required onChange={this.handleUserInput} />
                             </div>
                             <div className="form-row-reg">
-                                <input type="email" name="email2" placeholder="confirm email" required />
+                                <input type="email" name="verifyEmail" placeholder="confirm email" required onChange={this.handleUserInput} onKeyUp={this.Verification} />
                             </div>
                         </div>
 
@@ -53,26 +98,27 @@ class Register extends Component {
                             <div className="form-row-reg">
                                 <select >
                                     <label>Security Question</label>
-                                    <i class="fas fa-arrow-down"></i>
+                                    <i className="fas fa-arrow-down"></i>
                                     <option value="q1">What is your mother's maiden name?</option>
                                     <option value="q2">What's the name of your favorite pet?</option>
                                     <option value="q3">What's your favorite color?</option>
                                     <option value="q4">What's the your first teacher's last name?</option>
                                 </select>
 
-                                <input className="form-row-reg" type="text" placeholder="Answer" required />
+                                <input className="form-row-reg" type="text" placeholder="securityAnswer" required onChange={this.handleUserInput} />
                             </div>
                         </div>
                         <div className="form-group-reg">
-                            <input className="form-row-reg" type="number" placeholder="zip code" required />
+                            <input className="form-row-reg" type="number" placeholder="zipCode" required onChange={this.handleUserInput} />
                         </div>
 
                     </div>
                 </div>
                 <div className="footer-reg">
-                    <input type="submit" className="btn-reg" value="Register" />
+                    <input type="submit" className="btn-reg" value="Register" onClick={this.handleSubmit} />
 
                 </div>
+                {this.state.showError && (<input className="message-box" id="message" disabled={true} readOnly={true} value={this.state.verify} size="30" />)}
             </form >
 
         )
