72fb621f9fc5001830c4d2b01ddb55d132d91838
Nirma Perera <nirmaperera23@gmail.com>
2021-02-09 11:59:07 -0500

    refactor toggle login

--- src/components/credentials/ToggleLogin.js
+++ src/components/credentials/ToggleLogin.js
@@ -1,63 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Login from './login/Login';
 import Register from './register/Register';
 import ReactCardFlip from 'react-card-flip';
 
-
+import '../credentials/login/login.scss';
 import top_grid from '../../assets/images/topGrid.png';
 import bottom_grid from '../../assets/images/bottomGrid.png';
 
-import '../credentials/login/login.scss';
+const ToggleLogin = () => {
+	const [isFlipped, setIsFlipped] = useState(false);
+	const [isHover, setIsHover] = useState(false);
 
-class ToggleLogin extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			isFlipped: false,
-			isHover: false
-		};
-	}
-
-
-
-	handleClick = (e) => {
+	const handleClick = (e) => {
 		e.preventDefault();
-		this.setState(prevState => ({ isFlipped: !prevState.isFlipped }));
+		setIsFlipped(!isFlipped)
 	}
 
-	onHover = () => {
-		console.log('hi')
-		this.setState({ isHover: true });
-	}
+	return (
+		<div className="App">
+			<img alt="topGrid" className="topGrid" width="350px" src={top_grid} />
+			<img alt="topGridRight" className="topGridRight" width="350px" src={top_grid} />
 
-	onHoverLeave = () => {
-		this.setState({ isHover: false });
-	}
+			<div className="login">
+				<ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+					<div className="glassContainer">
+						<button className="flipbtn register" onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)} onClick={handleClick}>Register</button>
+						<Login />
+					</div>
 
-	render() {
-		return (
-			<div className="App">
-				<img className="topGrid" width="350px" src={top_grid} />
-
-				<img className="topGridRight" width="350px" src={top_grid} />
-				<div className="login">
-					<ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="horizontal">
-						<div className="glassContainer">
-							<button className="flipbtn register" onMouseEnter={this.onHover} onMouseLeave={this.onHoverLeave} onClick={this.handleClick}>Register</button>
-							<Login />
-						</div>
-
-						<div className="glassContainer">
-							<button className="flipbtn" id="login" onClick={this.handleClick}>Login</button>
-							<Register />
-						</div>
-					</ReactCardFlip>
-				</div>
-				<img className="bottomGrid" width="400px" src={bottom_grid} />
+					<div className="glassContainer">
+						<button className="flipbtn" id="login" onClick={handleClick}>Login</button>
+						<Register />
+					</div>
+				</ReactCardFlip>
 			</div>
-		);
-	}
-}
 
+			<img alt="bottomGrid" className="bottomGrid" width="400px" src={bottom_grid} />
+		</div>
+	)
+}
 
 export default ToggleLogin;
