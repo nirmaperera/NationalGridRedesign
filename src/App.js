913d47e460a393abc6a303a3fa4af4df7dd29a54
Nirma Perera <nirmaperera23@gmail.com>
2021-02-10 13:27:51 -0500

    clean up scss and removed unused images

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { connect } from 'react-redux';
 
 import Navigation from './components/navigation/Navigation';
 import PrivateRoute from './components/helpers/PrivateRoute';
-
 import ToggleLogin from "./components/credentials/ToggleLogin";
 import Dashboard from './components/dashboard/Dashboard';
 import Profile from './components/profile/Profile';
@@ -16,7 +15,6 @@ const App = (props) => {
 		<BrowserRouter>
 			<Navigation />
 			<Switch>
-
 				<Route exact path="/" component={ToggleLogin} />
 				<PrivateRoute exact path="/dashboard" component={Dashboard} isLogged={props.isLogged} />
 				<PrivateRoute exact path="/profile" component={Profile} isLogged={props.isLogged} />
--- src/components/helpers/EnterKeyPress.js
+++ src/components/helpers/EnterKeyPress.js
@@ -1,11 +1,11 @@
 const handleEnter = (e) => {
-    if (e.keyCode === 13) {
-        const form = e.target.form;
-        const index = Array.prototype.indexOf.call(form, e.target);
+	if (e.keyCode === 13) {
+		const form = e.target.form;
+		const index = Array.prototype.indexOf.call(form, e.target);
 
-        form.elements[index + 1].focus();
-        e.preventDefault();
-    }
+		form.elements[index + 1].focus();
+		e.preventDefault();
+	}
 
 }
 
